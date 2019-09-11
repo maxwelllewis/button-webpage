3326bd4cb680b91cbc17ceb6b5d9264f912dd644
Maxwell Briles <maxbriles@gmail.com>
2019-09-10 17:00:14 -0700

    add final js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(function()  {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
   $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
 });
